6ea21ffe0576eb7600ec5e077ceb0a06a0e886bd
Gaurav Sharma <dev.gaurav94@gmail.com>
2018-09-23 19:24:49 +0530

    more renaming

--- src/main/java/com/gaurav/rest/lambda/constants/ConfigurationConstants.java
+++ src/main/java/com/gaurav/rest/lambda/constants/ConfigurationConstants.java
@@ -7,9 +7,9 @@ import java.nio.file.Paths;
 public interface ConfigurationConstants {
 
 
-    String DEFAULT_CONFIG_DIR_PATH_LINUX = "/usr/local/var/Restify/Configuration";
-    String DEFAULT_LOG_DIR_PATH_LINUX = "/usr/local/var/Restify/Logs";
-    String DEFAULT_CONFIG_FILE_NAME = "Restify_Rest_jobs.xml";
+    String DEFAULT_CONFIG_DIR_PATH_LINUX = "/usr/local/var/rest-lambda/Configuration";
+    String DEFAULT_LOG_DIR_PATH_LINUX = "/usr/local/var/rest-lambda/Logs";
+    String DEFAULT_CONFIG_FILE_NAME = "rest_lambda_jobs.xml";
 
     Path REST_CONFIG_FILE = Paths.get(DEFAULT_CONFIG_DIR_PATH_LINUX + File.separator + DEFAULT_CONFIG_FILE_NAME);
     Path REST_LOG_PATH = Paths.get(DEFAULT_LOG_DIR_PATH_LINUX);
