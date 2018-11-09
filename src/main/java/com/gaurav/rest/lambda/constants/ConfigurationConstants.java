77663d629b07fb0a580b0d998792c42dba2a215d
Gaurav Sharma <dev.gaurav94@gmail.com>
2018-11-09 11:39:24 +0530

    log path

--- src/main/java/com/gaurav/rest/lambda/constants/ConfigurationConstants.java
+++ src/main/java/com/gaurav/rest/lambda/constants/ConfigurationConstants.java
@@ -14,8 +14,8 @@ import java.nio.file.Paths;
 public interface ConfigurationConstants {
 
 
-    String DEFAULT_CONFIG_DIR_PATH_LINUX = "/usr/local/var/rest-lambda/Configuration";
-    String DEFAULT_LOG_DIR_PATH_LINUX = "/usr/local/var/rest-lambda/Logs";
+    String DEFAULT_CONFIG_DIR_PATH_LINUX = "/var/rest-lambda/Configuration";
+    String DEFAULT_LOG_DIR_PATH_LINUX = "/var/rest-lambda/Logs";
     String DEFAULT_CONFIG_FILE_NAME = "rest_lambda_jobs.xml";
 
     Path REST_CONFIG_FILE = Paths.get(DEFAULT_CONFIG_DIR_PATH_LINUX + File.separator + DEFAULT_CONFIG_FILE_NAME);
