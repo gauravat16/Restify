ce9d7b2a48b01f5ef14a0eb531013e856b76ad94
Gaurav Sharma <dev.gaurav94@gmail.com>
2019-03-24 09:49:54 +0530

    added example tile, UI is tiled

---
+++ src/main/java/com/gaurav/rest/lambda/RestLambdaApp.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018.  Gaurav Sharma, All rights reserved.
+ */
+
+package com.gaurav.rest.lambda;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLambdaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestLambdaApp.class, args);
+    }
+
+}
